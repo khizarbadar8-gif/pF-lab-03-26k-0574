3a8b95be769557557b34ce4efdfe9aaf6bea5dec
khizarbadar8-gif <khizarbadar8@gmail.com>
2026-09-18 07:48:38 -0700

    Add labtask09.c with formatted output examples

---
+++ labtask09.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Line 1\nLine 2\n");
+    printf("Column1\tColumn2\tColumn3\n");
+    printf("Displaying a Backslash: \\\n");
+    printf("Displaying Double Quotes: \"Hello World\"\n");
+
+    return 0;
+}
